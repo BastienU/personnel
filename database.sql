528ac7aeabca6118f58964ed813e789fca6595ab
Adrien Boyer <103132622+Adriegabto@users.noreply.github.com>
2024-02-02 15:48:25 +0100

    maj database

--- database.sql
+++ database.sql
@@ -7,27 +7,19 @@ CREATE TABLE EMPLOYE
     prenom_employe VARCHAR(100),
     mail_employe VARCHAR(100) ,
     password_employe VARCHAR(100),
-    admin VARCHAR(100),
     date_arrivee DATE,
-    date_depart DATE
+    date_depart DATE,
+    id_ligue int,
 );
 
 CREATE TABLE LIGUE
 (
     id_ligue INT PRIMARY KEY NOT NULL,
-    nom_ligue
+    nom_ligue varchar(100),
+    id_employe varchar(100)
 );
 
 ALTER TABLE EMPLOYE
-ADD FOREIGN KEY id_ligue REFERENCES LIGUE;
+ADD FOREIGN KEY id_ligue REFERENCES LIGUE(id_ligue);
 
-ALTER TABLE EMPLOYE
-FOREIGN KEY (id_ligue)
-REFERENCES LIGUE(id_ligue);
-
-ALTER TABLE LIGUE
-ADD COLUMN id_employe INT;
-
-ALTER TABLE LIGUE
-FOREIGN KEY (id_employe)
-REFERENCES EMPLOYE(id_employe);
+ALTER TABLE LIGUE FOREIGN KEY (id_employe) REFERENCES EMPLOYE(id_employe);
